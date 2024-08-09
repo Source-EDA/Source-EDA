416c67e2304de7cf3544bffb36626db3a3bc2b3a
Adrien Luitot <23219818+adrienluitot@users.noreply.github.com>
2024-08-08 22:17:51 -0400

    remove build shit

--- build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_source_eda_FILE /Users/adrien/Documents/source_eda/dev/source_eda/build/Qt_6_7_2_for_macOS-Debug/source_eda.app/Contents/MacOS/source_eda)
-set(__QT_DEPLOY_TARGET_source_eda_TYPE EXECUTABLE)
